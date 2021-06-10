1ecff6572acc5952bdf6dc2cf9ba64fa3bf65ec4
Michelet <michelet@usp.br>
2021-06-10 09:46:27 -0300

    teste

--- frontend/components/ProfileCard.js
+++ frontend/components/ProfileCard.js
@@ -7,7 +7,9 @@ const ProfileCard = ({ profile }) => {
       <div className="mt-2 text-left">
         <div>
           <span className="text-xl">Username:</span>
-          <span className="text-lg text-green-700">{profile.username}</span>
+          <span className="text-xl text-blue-800 border border-black">
+            {profile.username}
+          </span>
         </div>
         <div>
           <span className="text-xl">Email:</span>
