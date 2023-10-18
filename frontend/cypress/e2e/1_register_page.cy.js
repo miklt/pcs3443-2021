ff8edef1f1047d1a2b8e0aa31ffad4628b9daa3c
Michelet Chávez <michelet@usp.br>
2023-10-18 13:17:03 -0300

    changes for presentation

--- frontend/cypress/e2e/1_register_page.cy.js
+++ frontend/cypress/e2e/1_register_page.cy.js
@@ -11,12 +11,20 @@ describe('A tela de Cadastro', () => {
     cy.get('form').submit()
     cy.url().should('eq', 'http://localhost:3000/')
   })
-  it('apresenta erro com usuário repetido', () => {
+  it('apresenta erro com nome de usuário repetido', () => {
     cy.visit('/register')
     cy.get('input[name=username]').type('michelet')
     cy.get('input[name=email]').type('michelet@usp.br')
     cy.get('input[name=password]').type('123456')
     cy.get('form').submit()
     cy.get('#status_message').should('have.text', 'Um usuário com esse login já existe.')
+  })
+  it('apresenta erro com nome de usuario diferente, porém com email repetido', () => {
+    cy.visit('/register')
+    cy.get('input[name=username]').type('outro')
+    cy.get('input[name=email]').type('michelet@usp.br')
+    cy.get('input[name=password]').type('123456')
+    cy.get('form').submit()
+    cy.get('#status_message').should('have.text', 'Um usuário com esse email já existe.')
   })
 })
